bb81e42162df139e9e1f56fa3e7b1be436c6ff55
LeandroViggiano <leandrom.v@outlook.com>
2022-10-06 17:59:43 -0300

    Ejercicio2 resuelto

--- Módulo Algoritmos/Ejercicio1/ejercicio1.js
+++ Módulo Algoritmos/Ejercicio1/ejercicio1.js
@@ -1,35 +1,32 @@
-var divresultado = document.getElementById("divresultado")
+var divresultado = document.getElementById("divresultado");
 var boton = document.getElementById("boton");
 var numero;
 function esPrimo(numero) {
-    var b=true
-    if (numero>1) {
-        for (let i = 2; i <= numero-1; i++) {
-            if (numero%i==0) {
-                b=false
-            }
-            
-        }
-    } else {
-        b=false
+  var b = true;
+  if (numero > 1) {
+    for (let i = 2; i <= numero - 1; i++) {
+      if (numero % i == 0) {
+        b = false;
+      }
     }
-    return b
-    
+  } else {
+    b = true;
+  }
+  return b;
 }
 
-boton.addEventListener("click", function (){
-    numero = parseInt(document.getElementById("numero").value);
-    var h = esPrimo(numero)
-    if (h) {
-        var resultado = document.createElement("p");
-        resultado.innerHTML = "El numero " + numero + " es primo"
-        divresultado.appendChild(resultado);
-        resultado.className = "text";
-        
-    } else {
-        var resultado = document.createElement("p");       
-        resultado.innerHTML = "El numero " + numero + " no es primo"
-        divresultado.appendChild(resultado);
-        resultado.className = "text";
-    }
+boton.addEventListener("click", function () {
+  numero = parseInt(document.getElementById("numero").value);
+  var h = esPrimo(numero);
+  if (h) {
+    var resultado = document.createElement("p");
+    resultado.innerHTML = "El numero " + numero + " es primo";
+    divresultado.appendChild(resultado);
+    resultado.className = "text";
+  } else {
+    var resultado = document.createElement("p");
+    resultado.innerHTML = "El numero " + numero + " no es primo";
+    divresultado.appendChild(resultado);
+    resultado.className = "text";
+  }
 });
---
+++ Módulo Algoritmos/Ejercicio2/ejercicio2.js
@@ -0,0 +1,53 @@
+/* Escribe una aplicación que solicite al usuario que ingrese una contraseña cualquiera.
+Después se le pedirá que ingrese nuevamente la contraseña, con 3 intentos. Cuando
+acierte ya no pedirá más la contraseña y mostrará un mensaje diciendo “Felicitaciones,
+recordás tu contraseña”. Si falla luego de 3 intentos se mostrará el mensaje “Tenes que
+ejercitar la memoria”. Los mensajes quedarán en pantalla a la espera que el usuario
+presione una tecla, luego de esto se cerrará el programa.
+ */
+
+var center = document.getElementById("center");
+var divresultado = document.getElementById("divresultado");
+var titulo = document.getElementById("titulo");
+var boton = document.getElementById("boton");
+var contrasena = "2";
+var contrasena1;
+var intentos = 0;
+var resultado;
+
+
+boton.addEventListener("click", function () {
+  intentos++;
+  if (contrasena == "2") {
+    contrasena1 = document.getElementById("contrasena").value;
+    resultado = document.createElement("h2");
+    divresultado.appendChild(resultado);
+    intentos = 0;
+    document.getElementById('contrasena').value = ''
+  }
+  
+  contrasena = document.getElementById("contrasena").value;
+  if (intentos===0) {
+    contrasena="1";
+  }
+  titulo.innerHTML = "Prueba de memoria. \n Volvé a ingresar tu contraseña";
+  boton.innerHTML = "Ingresar intento";
+
+  
+  if (contrasena === contrasena1) {
+    resultado.innerHTML = "Felicitaciones, recordás tu contraseña";
+    center.removeChild(boton);
+
+  }
+  if (intentos === 3) {
+    resultado.innerHTML = "Tenes que ejercitar la memoria";
+    center.removeChild(boton);
+
+  }
+  boton.setAttribute(
+  "onclick",
+  "document.getElementById('contrasena').value = ''"
+);
+console.log("0: "+contrasena);
+console.log("1: "+contrasena1);
+});
