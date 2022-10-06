3dd9c014727533d77435c3ceb91f7e71dd405096
LeandroViggiano <leandrom.v@outlook.com>
2022-10-06 14:09:11 -0300

    Primer ejercicio resuelto

---
+++ Módulo Algoritmos/Ejercicio1/ejercicio1.js
@@ -0,0 +1,35 @@
+var divresultado = document.getElementById("divresultado")
+var boton = document.getElementById("boton");
+var numero;
+function esPrimo(numero) {
+    var b=true
+    if (numero>1) {
+        for (let i = 2; i <= numero-1; i++) {
+            if (numero%i==0) {
+                b=false
+            }
+            
+        }
+    } else {
+        b=false
+    }
+    return b
+    
+}
+
+boton.addEventListener("click", function (){
+    numero = parseInt(document.getElementById("numero").value);
+    var h = esPrimo(numero)
+    if (h) {
+        var resultado = document.createElement("p");
+        resultado.innerHTML = "El numero " + numero + " es primo"
+        divresultado.appendChild(resultado);
+        resultado.className = "text";
+        
+    } else {
+        var resultado = document.createElement("p");       
+        resultado.innerHTML = "El numero " + numero + " no es primo"
+        divresultado.appendChild(resultado);
+        resultado.className = "text";
+    }
+});
